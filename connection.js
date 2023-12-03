f69ad7752a151fa13179b791724d7fdfe9002cae
Lucas Lima <lucaslcsteam@hotmail.com>
2023-12-03 15:18:34 -0300

    tete

--- connection.js
+++ connection.js
@@ -1,10 +1,15 @@
 import mysql from 'mysql2/promise';
+import url from 'url';
+
+const databaseUrl = process.env.DATABASE_URL;
+const parsedDatabaseUrl = url.parse(databaseUrl);
 
 const pool = mysql.createPool({
-  host: 'luczzzz.duckdns.org',
-  user: 'root',
-  password: 'lucas123',
-  database: 'ordem-sheet',
+  host: parsedDatabaseUrl.hostname,
+  user: parsedDatabaseUrl.auth.split(':')[0],
+  password: parsedDatabaseUrl.auth.split(':')[1],
+  database: parsedDatabaseUrl.pathname.substr(1),
+  port: parsedDatabaseUrl.port,
 });
 
 export default pool;
